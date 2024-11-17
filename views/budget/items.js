4bfa6c9257b746d8d753d27b32bb0036c10cee80
aisyah551 <nooraisyah551@gmail.com>
2024-11-17 23:10:29 +0800

    code optimization

--- views/budget/items.js
+++ views/budget/items.js
@@ -1,39 +1,47 @@
 ﻿<script type="text/javascript">//<![CDATA[
 $(function() {
+    // Cache frequently used selectors
+    const categoryRow = $("#budget_item_category_type__row");
+    const unitCostRow = $("#budget_item_unit_cost__row");
+    const monthlyCostRow = $("#budget_item_monthly_cost__row");
+    const minuteCostRow = $("#budget_item_minute_cost__row");
+    const megabyteCostRow = $("#budget_item_megabyte_cost__row");
+    const costTypeSelect = $("select[name='cost_type']");
+
     // Default to One-time costs
     // Hide the Running Cost inputs
-    $("#budget_item_monthly_cost__row").hide();
-    $("#budget_item_minute_cost__row").hide();
-    $("#budget_item_megabyte_cost__row").hide();
+    monthlyCostRow.hide();
+    minuteCostRow.hide();
+    megabyteCostRow.hide();
             
     // When the Cost type changes:
-	$("select[name='cost_type']").change(function() {
+	costTypeSelect.change(function() {
 		// What is the new cost type?
-        cost_type=$(this).val();
+        cost_type = $(this).val();
         if (cost_type==2) {
             // Hide the Category
-            $("#budget_item_category_type__row").hide();
+            categoryRow.hide();
             // Hide the Unit Cost input
-            $("#budget_item_unit_cost__row").hide();
+            unitCostRow.hide();
             // Show the Running Cost inputs
-            $("#budget_item_monthly_cost__row").show();
-            $("#budget_item_minute_cost__row").show();
-            $("#budget_item_megabyte_cost__row").show();
+            monthlyCostRow.show();
+            minuteCostRow.show();
+            megabyteCostRow.show();
         } else if (cost_type==1) {
             // Hide the Running Cost inputs
-            $("#budget_item_monthly_cost__row").hide();
-            $("#budget_item_minute_cost__row").hide();
-            $("#budget_item_megabyte_cost__row").hide();
+            monthlyCostRow.hide();
+            minuteCostRow.hide();
+            megabyteCostRow.hide();
             // Show the Category & Unit Cost input
-            $("#budget_item_category_type__row").show();
-            $("#budget_item_unit_cost__row").show();
+            categoryRow.show();
+            unitCostRow.show();
         }
 	})
     
     // Set unused values before submitting form
     $("input[type='submit']:last").click(function(event){
         // What is the final cost type?
-        cost_type=$("select[name='cost_type']").val();
+        cost_type = costTypeSelect.val();
         if (cost_type==2) {
             // Set the Category
             $('#budget_item_category_type').val("Running Cost")
@@ -46,7 +54,7 @@ $(function() {
             $('#budget_item_megabyte_cost').val('0.0')
         }
         // Pass to RESTlike CRUD controller
-        event.default();
+        event.preventDefault();
         return false;
     })
 });
